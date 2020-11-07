6541589236e145e46aaa1d7484d87b63dab4efdf
Annie Gao <qijia.gao@mail.utoronto.ca>
2020-11-06 22:27:35 -0500

    use -j to store entry in global journal in home dir

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,59 @@
 use std::env;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::fs;
 use std::io::prelude::*;
 use std::io::ErrorKind;
-use std::path::Path;
+use dirs::home_dir;
 
-const FILEPATH: &str = "jrnii.txt";
+const DIR_NAME: &str = ".jrnii";
+const FILE_NAME: &str = "jrnii.txt";
 
 fn main() {
+    let mut parent_path = home_dir().unwrap();
+    parent_path.push(DIR_NAME);   // file stored under $HOME/.jrnii 
+
     let mut args: Vec<String> = env::args().collect();
     if args.len() == 1 {
         println!("Starting a new jrnii? Type in a note about your day!");
-    }
-    let file_path = Path::new(FILEPATH);
+        return;
+    } 
 
-    let mut open_opts = match OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(file_path) {
-            Err(why) => match why.kind() {
-                ErrorKind::NotFound => match File::create(file_path) {
-                    Ok(fc) => fc,
-                    Err(e) => panic!("Error creating the file: {:?}", e),
+    if args[1] == "-j" {
+        let file_path = parent_path.join(FILE_NAME);
+
+        let mut open_opts = match OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(&file_path) {
+                Err(why) => match why.kind() {
+                    ErrorKind::NotFound => match fs::create_dir(&parent_path) {
+                        Ok(_) => File::create(&file_path).unwrap(),
+                        Err(e) => panic!("Error creating the directory: {:?}", e),
+                    },
+                    other_error => panic!("Error opening the file: {:?}", other_error)
                 },
-                other_error => {
-                    panic!("Error opening the file: {:?}", other_error)
+                Ok(opts) => {opts}
+            };
+
+        args.remove(0);
+        args.remove(0);
+        writeln!(open_opts, "{}", args.join(" ")).unwrap();
+
+    } else {
+        let mut open_opts = match OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(FILE_NAME) {
+                Err(why) => match why.kind() {
+                    ErrorKind::NotFound => File::create(FILE_NAME).unwrap(),
+                    other_error => panic!("Error opening the file: {:?}", other_error)
                 },
-            },
-            Ok(opts) => {opts}
-        };
-
-    args.remove(0);
-
-    let write_str = args.join(" ");
-    if let Err(e) = writeln!(open_opts, "{}", write_str) {
-        eprintln!("could nor write to file: {}", e)
+                Ok(opts) => {opts}
+            };
+        args.remove(0);
+        writeln!(open_opts, "{}", args.join(" ")).unwrap();
     }
+    
     // if second arg starts with -, then it is a flag, else it is new entry
 }
